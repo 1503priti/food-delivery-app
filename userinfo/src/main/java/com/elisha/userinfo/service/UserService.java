f09baefbb52e5021cca16e416895782e7dbd76fe
Priti Kumari <151085priti@gmail.com>
2025-02-15 13:44:14 +0800

    fetch user by Id api added

--- userinfo/src/main/java/com/elisha/userinfo/service/UserService.java
+++ userinfo/src/main/java/com/elisha/userinfo/service/UserService.java
@@ -5,7 +5,11 @@ import com.elisha.userinfo.entity.User;
 import com.elisha.userinfo.mapper.UserMapper;
 import com.elisha.userinfo.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +20,11 @@ public class UserService {
     public UserDTO addUser(UserDTO userDTO) {
        User savedUser =  userRepo.save(UserMapper.INSTANCE.mapUserDTOToUser(userDTO));
        return UserMapper.INSTANCE.mapUserToUserDTO(savedUser);
+    }
+
+    public ResponseEntity<UserDTO> fetchUserDetailsById(Integer userId) {
+        Optional<User> fetchedUser =  userRepo.findById(userId);
+        return fetchedUser.map(user -> new ResponseEntity<>(UserMapper.INSTANCE.mapUserToUserDTO(user), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.OK));
+
     }
 }
