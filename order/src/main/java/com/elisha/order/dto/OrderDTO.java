9d8db075ad2fb37822cf3be3375cfadacaf945fa
Priti Kumari <151085priti@gmail.com>
2025-02-18 18:37:47 +0800

    entity added

---
+++ order/src/main/java/com/elisha/order/dto/OrderDTO.java
@@ -0,0 +1,16 @@
+package com.elisha.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderDTO {
+   private List<FoodItemsDTO> foodItemsList;
+    private Integer userId;
+    private Restaurant restaurant;
+}
